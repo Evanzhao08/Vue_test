ac4f16cf7bfb9e723339ea3084e975aaf34a13d3
Evanzhao08 <15309299853@189.cn>
2021-12-05 23:17:18 +0800

    update Code

--- src/components/School.vue
+++ src/components/School.vue
@@ -1,13 +1,14 @@
 <!--
  * @Author: Evan Zhao
  * @Date: 2021-12-05 21:37:33
- * @LastEditTime: 2021-12-05 23:10:17
+ * @LastEditTime: 2021-12-05 23:17:02
  * @FilePath: \Vue_test1\src\components\School.vue
 -->
 <template>
   <div class="school">
       <h2>学校名称:{{name}}</h2>
       <h2>学校地址：{{address}}</h2>
+      
   </div>
 </template>
 
