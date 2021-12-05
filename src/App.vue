d347af04c41a1ba2351a2f52188837ff6c7022db
Evanzhao08 <15309299853@189.cn>
2021-12-05 23:29:11 +0800

    pros 属性

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,19 @@
 <!--
  * @Author: Evan Zhao
  * @Date: 2021-12-05 21:33:23
- * @LastEditTime: 2021-12-05 23:00:26
+ * @LastEditTime: 2021-12-05 23:26:29
  * @FilePath: \Vue_test1\src\App.vue
 -->
 <template>
   <div>
-      <h1 v-text="msg" ref="title"></h1>
-      <button ref="btn" @click="showDOM">点我输出上方的消息</button>
-      <School ref="sch"/>
+      <Student ref="sch"/>
   </div>
 </template>
 
 <script>
-    import School from './components/School.vue'
+    import Student from './components/Student.vue'
     export default {
         name:'App',
-        components:{School},
-        data() {
-            return {
-                msg:"欢迎学习Vue!"
-            }
-        },
-        methods: {
-            showDOM(){
-                console.log(this.$refs.title);
-                console.log(this.$refs.btn);
-                console.log(this.$refs.sch);
-            }
-        }
-        
+        components:{Student},
     }
 </script>
---
+++ src/components/Student.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: Evan Zhao
+ * @Date: 2021-12-05 21:37:33
+ * @LastEditTime: 2021-12-05 23:28:14
+ * @FilePath: \Vue_test1\src\components\Student.vue
+-->
+<template>
+  <div>
+      <h1>{{msg}}</h1>
+      <h2>学生姓名:{{name}}</h2>
+      <h2>学生性别:{{sex}}</h2>
+      <h2>学生年龄:{{age}}</h2>
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'Student',
+        data() {
+            return {
+                msg:'我是一个学生',
+                name:'张三',
+                sex:'男',
+                age:18,
+            }
+        },
+    }
+</script>
+
