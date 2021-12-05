b7eda8968995251eb8c7ae5eb0541dfcb3c373dc
Evanzhao08 <15309299853@189.cn>
2021-12-05 23:57:01 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <!--
  * @Author: Evan Zhao
  * @Date: 2021-12-05 21:33:23
- * @LastEditTime: 2021-12-05 23:35:11
+ * @LastEditTime: 2021-12-05 23:56:24
  * @FilePath: \Vue_test1\src\App.vue
 -->
 <template>
   <div>
       <Student name="李四" sex="女" age="18"/>
+      
   </div>
 </template>
 
