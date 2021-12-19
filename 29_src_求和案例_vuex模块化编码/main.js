db089393187a825ece5bb053eb5cca37117f913d
Evanzhao08 <15309299853@189.cn>
2021-12-19 17:00:08 +0800

    Router 传参

---
+++ 29_src_求和案例_vuex模块化编码/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+// 引入插件
+import vueResource from 'vue-resource'
+// 引入store
+import store from './store'
+
+Vue.config.productionTip = false
+// 使用插件
+Vue.use(vueResource)
+//创建vm
+new Vue({
+    el:'#app',
+    store,
+    render:h =>h(App),
+    beforeCreate() {
+        Vue.prototype.$bus = this
+    },
+})
---
+++ src/pages/Message.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="{id,title} in messageList" :key="id">
+        <!-- 跳转路由 并携带query参数，to的字符串写法-->
+        <!-- <router-link :to="`/home/message/detail?id=${id}&title=${title}`">{{title}}</router-link>&nbsp;&nbsp; -->
+        <!-- 跳转路由 并携带query参数，to的对象写法-->
+        <!-- 跳转路由 并携带params参数-->
+        <!-- <router-link :to="`/home/message/detail/${id}/${title}`">{{title}}</router-link> -->
+        <router-link :to="{
+           name:'xiangqing',
+            query:{
+              id,
+              title
+              }
+          }"> 
+          {{title}}
+        </router-link>
+      </li>
+    </ul>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Message',
+     data() {
+    return {
+      messageList:[
+        {id:'001',title:'消息001'},
+        {id:'002',title:'消息002'},
+        {id:'003',title:'消息003'},
+      ]
+    }
+  },
+};
+</script>
