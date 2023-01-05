75ebf31fa381295fdce063c02dd30c2771944e67
Evanzhao08 <15309299853@189.cn>
2023-01-05 17:51:19 +0800

    update color

--- 10_src组件自定义事件/App.vue
+++ 10_src组件自定义事件/App.vue
@@ -6,37 +6,37 @@
 -->
 <template>
   <div>
-      <!-- <Student @testSend="getStudentName"/> -->
-      <Student ref="student"/>
-      <School :getSchoolName="getSchoolName"/>
+    <!-- <Student @testSend="getStudentName"/> -->
+    <Student ref="student" />
+    <School :getSchoolName="getSchoolName" />
   </div>
 </template>
 
 <script>
-    import Student from './components/Student.vue'
-    import School from './components/School.vue'
-    export default {
-        name:'App',
-        components:{Student,School},
-        methods: {
-            getSchoolName(name){
-                console.log(name)
-            },
-            getStudentName(name,...params){
-                console.log(name,params)
-            }
-        },
-        mounted(){     
-             this.$refs.student.$on('testSend',this.getStudentName)
-             this.$refs.student.$on('demo',this.getStudentName)
-            //触发一次不在执行
-            //this.$refs.student.$once('testSend',this.getStudentName)
-        }
-    }
+import Student from "./components/Student.vue";
+import School from "./components/School.vue";
+export default {
+  name: "App",
+  components: { Student, School },
+  methods: {
+    getSchoolName(name) {
+      console.log(name);
+    },
+    getStudentName(name, ...params) {
+      console.log(name, params);
+    },
+  },
+  mounted() {
+    this.$refs.student.$on("testSend", this.getStudentName);
+    this.$refs.student.$on("demo", this.getStudentName);
+    //触发一次不在执行
+    //this.$refs.student.$once('testSend',this.getStudentName)
+  },
+};
 </script>
 
 <style>
-.title{
-    color: red;
+.title {
+  color: rgb(185, 20, 20);
 }
 </style>
